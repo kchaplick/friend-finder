5c68c901abaf5f2fe14451e17f5e9aebb3958f37
Kasie <kchaplick92@gmail.com>
2019-05-07 16:22:21 -0400

    routing

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+
+
+var friends = [
+  {
+    name: "Kasie Chaplick",
+    photoLink: "https://www.pexels.com/photo/man-wearing-dress-shirt-holding-dandelion-1250426/",
+    answers: [1,3,4,5,4,2,3,1,2,3],
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
